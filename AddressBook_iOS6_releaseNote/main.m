2cf9f780b02663f09570e257cd16adb64722471a
Deepak Vishwakarma <deepak.vishwakarma@globallogic.com>
2013-01-29 12:46:02 +0530

    Related Changes

---
+++ AddressBook_iOS6_releaseNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AddressBook_iOS6_releaseNote
+//
+//  Created by Global Logic on 28/01/13.
+//  Copyright (c) 2013 Celestix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CAAppDelegate class]));
+    }
+}
